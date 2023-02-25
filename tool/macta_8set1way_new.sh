4067d5574bf95842710509e677e39559f0cb2d20
Mulong Luo <ml2558@cornell.edu>
2023-02-25 16:31:30 -0500

    add new macta trace

---
+++ tool/macta_8set1way_new.sh
@@ -0,0 +1,78 @@
+# remember to use 
+# modprobe msr
+# wrmsr -a 0xc90 0x0001
+# use Intel CAT to set L3 one way
+
+# v 9 8 v 8 9  r r r 10 11 13 15 v 10 11 13 15 r 14 v 14
+# r 12 v 13 v 13 12 v v 10 v 10 13 11 v 11 r 9 v 9 15 v 15 14 v 14
+# 
+
+total_number=0
+correct_numer=0
+for i in `seq 0 124`; do
+	for v in `seq 0 7`; do
+		total_number=$((${total_number} + 1))
+		r1=$(($RANDOM%8)) 
+		r2=$(($RANDOM%8)) 
+		r3=$(($RANDOM%8)) 
+		r4=$(($RANDOM%8)) 
+		#z=$(./cachequery.py -l l3 -r 0_${v} 1_1 1_0 0_${v} 1_0? 1_1? 0_${r1} 0_${r2} 0_${r3} 1_2 1_3 1_5 1_7 0_${v} 1_2? 1_3? 1_5? 1_7? 0_${r4} 1_6 0_${v} 1_6? | tail -1 | awk '{print  $24 "\t" $25  "\t"  $26 "\t" $27 "\t" $28 "\t" $29 "\t" $30 }')
+		z=$(./cachequery.py -l l3 -r 0_${r1} 1_4 0_${v} 1_5 0_${v} 1_5? 1_4? 0_${v} 0_${v} 1_2 0_${v} 1_2?  1_5 1_3 0_${v} 1_3? 0_${r2} 1_1 0_${v} 1_1? 1_7 0_${v} 1_7? 1_6 0_${v} 1_6? | tail -1 | awk '{print $28 "\t" $29  "\t"  $30 "\t" $31 "\t" $32 "\t" $33 "\t" $34 }')
+		echo $z	
+
+		l1=$(echo $z | awk '{print $1}')
+		l2=$(echo $z | awk '{print $2}')
+		l3=$(echo $z | awk '{print $3}')
+		l4=$(echo $z | awk '{print $4}')
+		l5=$(echo $z | awk '{print $5}')
+		l6=$(echo $z | awk '{print $6}')
+		l7=$(echo $z | awk '{print $7}')
+	
+		####echo $l1
+		####echo $l2
+		####echo $l3
+		####echo $l4
+		####echo $l5
+		####echo $l6
+		####echo $l7
+		
+		
+		if [ $v -eq 0 ] && [ $l1 -ne 0 ]  && [ $l2 -ne 0 ]   && [ $l3 -ne 0 ] && [ $l4 -ne 0 ]&& [ $l5 -ne 0 ] && [ $l6 -ne 0 ]&& [ $l7 -ne 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 1 ] && [ $l5 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 2 ] && [ $l3 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 3 ] && [ $l4 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 4 ] && [ $l2 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 5 ] && [ $l1 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 6 ] && [ $l7 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+		if [ $v -eq 7 ] && [ $l6 -eq 0 ]
+		then
+			correct_number=$((${correct_number} + 1))
+		fi
+
+
+	done
+done
+
+echo ${correct_number}
+echo ${total_number}
